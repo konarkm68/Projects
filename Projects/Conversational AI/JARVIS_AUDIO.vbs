dafe9d189f38cbcefda30dae86cdbc3fad33955f
Konark Mittal <49273687+konarkm68@users.noreply.github.com>
2023-12-01 19:15:21 +0530

    restructuring

--- Projects/Conversational AI/JARVIS_AUDIO.vbs
+++
@@ -1,1 +0,0 @@
-CreateObject("SAPI.Spvoice").Speak"Hello there, I am JARVIS; Konark Mittal's Personal Virtual Assistant"
--- Projects/Conversational AI/TTS.py
+++
@@ -1,7 +0,0 @@
-import os
-message=input('Enter text to convert to speech: ')
-file=open('TTS.vbs','w')
-with file as source:
-    cmd='CreateObject("SAPI.Spvoice").Speak"'+message+'"'
-    file.write(cmd)
-os.system('TTS.vbs')
