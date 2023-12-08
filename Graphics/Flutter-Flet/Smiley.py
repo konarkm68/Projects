0941a5c64d3a53931dd8ce704bd6a4f0e7147acc
Konark Mittal <info.kmittal@gmail.com>
2023-12-08 20:26:48 +0530

    update + RS

--- Graphics/Flutter-Flet/Smiley.py
+++ Graphics/Flutter-Flet/Smiley.py
@@ -15,7 +15,7 @@ def main(page: flet.Page):
             flet.canvas.Circle(120, 90, 5, stroke_paint),
             flet.canvas.Circle(120, 90, 4, fill_paint),
             ## Smile
-            flet.canvas.Arc(70, 95, 60, 40, 0, math.pi, paint=stroke_paint), # x, y, width, height, start_angle, sweep_angle, paint
+            flet.canvas.Arc(70, 98, 60, 36, 0, math.pi, paint=stroke_paint), # x, y, width, height, start_angle, sweep_angle, paint
         ],
         width=float("inf"), expand=True,
     )
--- Tools/GUI/generate-ico/generate-ico.py
+++ Tools/GUI/generate-ico/generate-ico.py
@@ -2,8 +2,6 @@ import asyncio
 from favicons import Favicons
 from tkinter import filedialog
 
-loop = asyncio.get_event_loop()
-
 YOUR_ICON = filedialog.askopenfilename(title='Select image file to generate favicon files:', filetypes=[('Image Files',
 ['.svg', '.jpeg', '.jpg', '.png', '.tiff', '.tif'])]) # img file location
 WEB_SERVER_ROOT = filedialog.askdirectory(title='Select folder to save favicon files:') # destination folder for files location
@@ -41,6 +39,8 @@ async def json_fvcon():
         await favicons.generate()
         as_json = favicons.json(indent=2)
         print(as_json)
+
+loop = asyncio.get_event_loop()
 
 loop.run_until_complete(async_fvcon())
 loop.run_until_complete(html_fvcon())
